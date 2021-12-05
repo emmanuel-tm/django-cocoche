11c9151854ada3f10219a1ecfbffff198ffe7b69
Emmanuel Torres Molina <eotorresmolina@gmail.com>
2021-12-05 17:26:59 -0300

    Configured django rest-framework

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -52,8 +52,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Here are my local apps:
-    'applications.cocoche'
+    'applications.cocoche',
+    # Third party apps:
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    ),
+
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend')
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
