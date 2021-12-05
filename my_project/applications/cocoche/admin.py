954fc3f084f0577877b701884c36b5a67d04106e
Emmanuel Torres Molina <eotorresmolina@gmail.com>
2021-12-05 13:58:03 -0300

    Verbose name of the database columns were modified and django admin was updated

--- my_project/applications/cocoche/admin.py
+++ my_project/applications/cocoche/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+# Import my models:
+from applications.cocoche.models import *
+
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
--- my_project/applications/cocoche/models.py
+++ my_project/applications/cocoche/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     id = models.BigAutoField(db_column='ID', primary_key=True)
-    name = models.CharField(verbose_name='titles', max_length=120, default='')
-    phone = models.CharField(verbose_name='phones', max_length=30, default='0000000000')
-    email = models.EmailField(verbose_name='emails', unique=True)
-    random_id = models.CharField(verbose_name='random ids', max_length=100, unique=True)
+    name = models.CharField(verbose_name='name', max_length=120, default='')
+    phone = models.CharField(verbose_name='phone', max_length=30, default='0000000000')
+    email = models.EmailField(verbose_name='email', unique=True)
+    random_id = models.CharField(verbose_name='random id', max_length=100, unique=True)
     createAt = models.CharField(verbose_name='create at', max_length=10, default='dd-mm-AAAA')
 
     class Meta:
