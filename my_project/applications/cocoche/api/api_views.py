3c3a4a6a4d87e6aed65533fffe405c79eaf89fe7
Emmanuel Torres Molina <eotorresmolina@gmail.com>
2021-12-07 11:00:52 -0300

    Override POST method - CreateUser API View

--- my_project/applications/cocoche/api/api_views.py
+++ my_project/applications/cocoche/api/api_views.py
@@ -1,14 +1,16 @@
-from typing import List
-
-from rest_framework import permissions
-from rest_framework.decorators import permission_classes
-from rest_framework.settings import perform_import
 # NOTE: import my serializers and my models.
+from django.http import response
 from applications.cocoche.api.serializers import *
 from applications.cocoche.models import *
 
 from rest_framework.permissions import IsAdminUser
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+
+import hashlib
+from datetime import datetime
 
 
 class GetCarsListAPIView (ListAPIView):
@@ -20,6 +22,7 @@ class GetCarsListAPIView (ListAPIView):
     queryset = CarsList.objects.all()
     serializer_class = CarsListSerializer
     permission_classes = []
+
 
 class GetFordCarsAPIView(ListAPIView):
     '''
@@ -41,3 +44,75 @@ class GetFordCarsAPIView(ListAPIView):
             return queryset
         except Exception as error:
             return {'error': f'Ha ocurrido el siguiente error: {error}'}
+
+
+class CreateUserAPIView(APIView):
+    '''
+    [METODO HTTP: POST]
+    \nVista de API Genérica-Personalizada basadas en Clases que
+    \n recibe peticiones de tipo "POST".
+    \nSchema:
+    \n{
+    \n  "name": "string", 
+    \n  "phone": "string", 
+    \n  "email": "string"
+    \n}
+    '''
+
+    permission_classes = []
+    authentication_classes = []
+    parser_classes = [JSONParser]
+    
+    def post(self, request, format=None):
+
+        user_data = {}
+
+        # NOTE:I get the parameters that arrive by METHOD HTTP: POST.
+        name = request.data.get('name')
+        phone = request.data.get('phone')
+        email = request.data.get('email')
+
+        serializer = UserSerializer(data=request.data)
+
+        try:
+            # Get User
+            user = User.objects.get(email=email)
+
+        # NOTE: If does not exist enter:
+        except User.DoesNotExist:
+            try:
+                random_id = hashlib.md5(email.encode()).hexdigest()
+                today = datetime.now()
+                today_str = today.strftime('%d-%m-%Y')
+
+            # NOTE: In case an exception ocurrs: for example: "No parameter is entered"
+            except Exception as error:
+                user_data = {'code': 500, 'message': 'missing data'}
+                return Response(data=user_data)
+
+            if serializer.is_valid() and type(phone) == str and phone.isdigit() and type(name) == str:
+                # Create a new User object:
+                user = User.objects.create(
+                    name = name,
+                    phone = phone,
+                    email = email,
+                    random_id = random_id,
+                    createAt = today_str
+                )
+
+                user.save()
+
+                user_data['id'] = user.random_id
+                user_data['createAt'] = user.createAt
+                return Response(data=user_data)
+
+            else:
+                user_data = {'code': 500, 'message': 'data wrongly entered'}
+                return Response(data=user_data)
+
+        except Exception as error:
+            user_data = {'code': 500, 'message': f'{error}'}
+            return Response(data=user_data)
+        
+        user_data = {'code': 500, 'message': 'email already registered'}
+        return Response(data=user_data)
--- my_project/applications/cocoche/api/urls.py
+++ my_project/applications/cocoche/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('get_cars/', get_cars),
     path('get_cars/list', GetCarsListAPIView.as_view()),
     path('get_ford_cars', GetFordCarsAPIView.as_view()),
+    path('create_user', CreateUserAPIView.as_view()),
 ]
